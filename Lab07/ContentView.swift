7bf908df5e6e539579437ee7994f088c0455cb4d
GusJTT <gturpot@unsa.edu.pe>
2024-11-12 21:17:33 -0500

    Initial Commit

---
+++ Lab07/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab07
+//
+//  Created by Gustavo on 12/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab07/Lab07App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab07App.swift
+//  Lab07
+//
+//  Created by Gustavo on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
